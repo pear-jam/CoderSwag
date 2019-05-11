fe5801395cd3e80f8ab6bfbf6f9d4bf3d71393eb
pearjam <samgriffiths7@gmail.com>
2019-05-11 15:02:30 +0200

    lesson03 complete

--- app/src/main/java/samuel/griffiths/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/samuel/griffiths/coderswag/Controller/MainActivity.kt
@@ -4,22 +4,20 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_main.*
+import samuel.griffiths.coderswag.Adapter.CategoryAdapter
 import samuel.griffiths.coderswag.Model.Category
 import samuel.griffiths.coderswag.R
 import samuel.griffiths.coderswag.Services.DataService
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter : ArrayAdapter<Category>
+    lateinit var adapter : CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = ArrayAdapter(this,
-            android.R.layout.simple_list_item_1,
-            DataService.categories)
-
+        adapter = CategoryAdapter(this, DataService.categories)
         categoryListView.adapter = adapter
 
     }
